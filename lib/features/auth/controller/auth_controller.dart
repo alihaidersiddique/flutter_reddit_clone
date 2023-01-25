10f22cab6ff2f317dcb566813dbeaa4f8ab77667
Muhammad Ali Haider <alihaidersiddique@hotmail.com>
2023-01-25 07:37:56 +0500

    google sign_in

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/utils.dart';
+import 'package:reddit_clone/features/auth/repository/auth_repository.dart';
+import 'package:reddit_clone/models/user_model.dart';
+
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
+final authControllerProvider = Provider(
+  (ref) => AuthController(
+    authRepository: ref.read(authRepositoryProvider),
+    ref: ref,
+  ),
+);
+
+class AuthController {
+  final AuthRepository _authRepository;
+  final Ref _ref;
+
+  AuthController({required AuthRepository authRepository, required Ref ref})
+      : _authRepository = authRepository,
+        _ref = ref;
+
+  signInWithGoogle(BuildContext context) async {
+    final user = await _authRepository.signInWithGoogle();
+    user.fold(
+      (l) => showSnackBar(context, l.message),
+      (userModel) =>
+          _ref.read(userProvider.notifier).update((state) => userModel),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/firebase_options.dart';
+import 'package:reddit_clone/theme/pallete.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,9 +23,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: Pallete.darkModeAppTheme,
+      home: LoginScreen(),
     );
   }
 }
